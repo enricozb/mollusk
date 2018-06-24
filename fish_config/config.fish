08f9b9b22ac91e01ffe85baca0107fb77d5b8d01
Enrico Borba <enricozb@gmail.com>
2018-06-24 11:43:20 +0900

    Code simplification
    - Combined mollusk & mollusk-relay, now mollusk can be used with a
      --relay mode.
    - Improved checking for exit.
    - cleaned up config.fish

--- fish_config/config.fish
+++ fish_config/config.fish
@@ -1,10 +1,10 @@
 # mollusk
-function preexec_test --on-event fish_preexec
+function mollusk_relay --on-event fish_preexec
     if test -n "$MOLLUSK"
         switch $argv
           case ".." clear ls cd mv "ls *" "mv *" "cd *"
           case "*"
-            mollusk-relay $argv "$MOLLUSK"
+            mollusk --relay $argv "$MOLLUSK"
             exit
         end
     end
